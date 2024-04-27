0784c9949ca2429151277eb8935a013cdf127784
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-04-27 09:14:48 +0530

    Auth APIs added

--- apis.js
+++ apis.js
@@ -1,38 +1,74 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors');
+
+app.use(cors());
+
+JWT_TOKEN = "IuXno8HwSauaVZ7eK+cOvYQ8Qxoc80RhJYKX/twR/EORogFY1wb/ct5aTnvWgwQxrdk47+HDKg/CE0qah2wxMw=="
+
+user_token = "eyJhbGciOiJIUzI1NiIsImtpZCI6InZObWF5NXpMTUdEQWZPWlIiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzE0MDE2MTcwLCJpYXQiOjE3MTQwMTI1NzAsImlzcyI6Imh0dHBzOi8vanFzcWpyY2F4bnhqcnFlbm5obWsuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6ImIzZWIyZjExLTA5NjAtNDFlZS1iMDQ1LTlkZmJkYjYxN2MxNCIsImVtYWlsIjoiYmhhcnRpeWFhbnNodWxAZ21haWwuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6e30sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoib3RwIiwidGltZXN0YW1wIjoxNzE0MDEyNTcwfV0sInNlc3Npb25faWQiOiJhZGFhNWIxYy0xMGM4LTQ3YzktOWE5OC05MWVmMmFiMTYxNDIiLCJpc19hbm9ueW1vdXMiOmZhbHNlfQ.UK87sMsBsK6IcxnwHnfSzv5NFF8cSK290UJadLMrCy0&expires_at=1714016170&expires_in=3600&refresh_token=0gp2oviYa1IbP5zGuVmvcg&token_type=bearer&type=magiclink"
 
 app.use(express.json());
 
 const { createClient } = require('@supabase/supabase-js');
 
-// Create a single supabase client for interacting with your database
 const supabase = createClient('https://jqsqjrcaxnxjrqennhmk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impxc3FqcmNheG54anJxZW5uaG1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1MTI3MjEsImV4cCI6MjAyOTA4ODcyMX0.19Y3Zjd72vKn6S9Us2djfjj7BUDSkkqBwbu6j8y2ug4')
 
-app.post('/post/', async (req,res)=>{
-    const {id, title, task } = req.body
+app.post('/post/', async (req, res) => {
+
+    const { id, title, task } = req.body
     const { error } = await supabase
         .from('Task')
         .insert({ id: id, title: title, task: task })
-    console.log(title,task,error);
+    console.log(title, task, error);
 
     res.send(
         {
-        "status": 201,
-        "statusText": "Created"
+            "status": 200,
+            "statusText": JSON.stringify({ id: id, title: title, task: task })
         }
-        )
+    )
 })
+
+// app.post('/post/', async (req,res)=>{
+
+//     var token = req.headers['x-access-token'];
+
+
+//     if(supabase.auth.signInWithIdToken(token)){
+//         const {id, title, task } = req.body
+//         const { error } = await supabase
+//             .from('Task')
+//             .insert({ id: id, title: title, task: task })
+//         console.log(title,task,error);
+//     }
+//     else{
+//         res.send(
+//             {
+//             "status": 400,
+//             "statusText": "User is not Authorised"
+//             }
+//             )
+//     }
+//     supabase.auth.getUser("jwt")
+
+//     res.send(
+//         {
+//         "status": 201,
+//         "statusText": JSON.stringify({id: id, title: title, task: task})
+//         }
+//         )
+// })
 
 app.get('/post/', async (req, res) => {
 
+    const { data, error } = await supabase
+        .from('Task')
+        .select('*')
 
-        const { data, error } = await supabase
-            .from('Task')
-            .select('*')
-
-            res.send(data)
-    }
+    res.send(data)
+}
 )
 
 app.get('/post/Id/', async (req, res) => {
@@ -40,32 +76,35 @@ app.get('/post/Id/', async (req, res) => {
     const { id, title } = req.query
     let query = supabase.from('Task').select()
 
-        if(id!=null){
-            query = query.eq('id',id)
-        }
-        else{
-            query = query.eq('title',title)
-        }
+    if (id != null) {
+        query = query.eq('id', id)
+    }
+    else {
+        query = query.eq('title', title)
+    }
 
-        const {error ,data} = await query
-        res.send(data)
+    const { error, data } = await query
+    res.send(data)
 })
 
 app.delete('/post/', async (req, res) => {
 
     const { id } = req.query
+    console.log(id)
     const { error } = await supabase
         .from('Task')
         .delete()
         .eq('id', id)
-        
-        res.send(
-            {
-                "status": 204,
-                "message": "Task Deleted!"
-            }
-        )
-    }        
+
+        console.log("deleted")
+
+    res.send(
+        {
+            "status": 204,
+            "message": "Task Deleted!"
+        }
+    )
+}
 )
 
 app.put('/post/', async (req, res) => {
@@ -75,13 +114,13 @@ app.put('/post/', async (req, res) => {
         .from('Task')
         .update({ is_completed: true })
         .eq('id', id)
-        
-        res.send(
-            {
-                "status": 204,
-                "message": "Task Completed"
-            }
-        )
+
+    res.send(
+        {
+            "status": 204,
+            "message": "Task Completed"
+        }
+    )
 })
 
 app.listen(port, () => {
